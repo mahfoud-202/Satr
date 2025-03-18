3be3c9064c8fe15f53f6feadd458f798bd69bde6
mahfoud-202 <166753586+mahfoud-202@users.noreply.github.com>
2025-03-18 02:47:56 +0100

    updates

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -1,1 +1,21 @@
-<h1>Hello, World!</h1>
+<script>
+    import TopBar from "./components/TopBar.svelte";
+    import Workspace from "./components/Workspace.svelte";
+    import StatusBar from "./components/StatusBar.svelte";
+</script>
+
+<div class="app">
+    <TopBar />
+    <Workspace />
+    <StatusBar />
+</div>
+
+<style>
+    .app {
+        display: grid;
+        grid-template-rows: 40px minmax(0, 1fr) 40px;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+</style>
---
+++ src/lib/components/TopBar.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<div class="title-bar"> Satr </div>
+
+<style>
+    .title-bar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid white;
+        width: 100%;
+        height: 100%;
+    }
+</style>
